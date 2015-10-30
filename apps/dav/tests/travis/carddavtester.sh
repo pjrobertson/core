5b470668a5cb372324f564613ec49b1369c07f06
Thomas Müller <thomas.mueller@tmit.eu>
2015-10-31 00:49:39 +0100

    Fix clone

--- apps/dav/tests/travis/carddavtester.sh
+++ apps/dav/tests/travis/carddavtester.sh
@@ -8,6 +8,7 @@ php -S 127.0.0.1:8888 -t "$SCRIPTPATH/../../../.." &
 
 
 if [ ! -f CalDAVTester/run.py ]; then
+	cd "$SCRIPTPATH"
     git clone https://github.com/DeepDiver1975/CalDAVTester.git
 	cd "$SCRIPTPATH/CalDAVTester"
     python run.py -s
